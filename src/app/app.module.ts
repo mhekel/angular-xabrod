2b2d0f64887b20994a1866f2aea774fa549dd3dd
mhekel <52319691+mhekel@users.noreply.github.com>
2021-12-12 21:56:03 -0800

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { HelloWorldTemplateComponent } from './hello-world-template.component';
-import { HelloWorldNgIfComponent } from './hello-world-ngif/hello-world-ngif.component';
-import { HelloWorldDependencyInjectionComponent } from './hello-world-di/hello-world-di.component';
-import { HelloWorldInterpolationComponent } from './hello-world-interpolation/hello-world-interpolation.component';
-import { HelloWorldBindingsComponent } from './hello-world-bindings/hello-world-bindings.component';
-
-
+import { FabricjsEditorModule } from 'projects/angular-editor-fabric-js/src/public-api';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent,
-    HelloWorldTemplateComponent,
-    HelloWorldNgIfComponent,
-    HelloWorldDependencyInjectionComponent,
-    HelloWorldInterpolationComponent,
-    HelloWorldBindingsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FabricjsEditorModule,
+    FormsModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
---
+++ src/assets/_vti_cnf/fabricDirtyStatus.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 Nov 2021 12:33:25 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|STOOPID\\mheke
+vti_modifiedby:SR|STOOPID\\mheke
+vti_timecreated:TR|30 Nov 2021 12:33:25 -0000
+vti_cacheddtm:TX|30 Nov 2021 12:33:25 -0000
+vti_filesize:IR|678
+vti_backlinkinfo:VX|
